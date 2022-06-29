a8e15357874ca7d999edb534bb13ba630f99def2
Fishmen <adam_phoz18@hotmail.com>
2022-06-30 09:21:38 +1000

    random quote generator complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React, {useState} from 'react';
+
+const quoteBank = [
+  {
+    quote: 'The greatest glory in living lies not in never falling, but in rising every time we fall.',
+    author: 'Nelson Mandela'
+  },
+  {
+    quote: 'The way to get started is to quit talking and begin doing.',
+    author: 'Walt Disney'
+  },
+  {
+    quote: "Your time is limited, so don't waste it living someone else's life. Don't be trapped by dogma, which is living with the results of other people's thinking.",
+    author: 'Steve Jobs'
+  },
+]
 
 function App() {
+  const num = Math.floor(Math.random() * quoteBank.length);
+  const [quote, setQuote] = useState({quote: quoteBank[num].quote, author: quoteBank[num].author});
+
+  function generateQuote() {
+    setQuote(quoteBank[Math.floor(Math.random() * quoteBank.length)]);    
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div className='wrapper' id='quote-box'>
+      <div id='text'><p><i class="fa-solid fa-quote-left"></i> {quote.quote}</p></div>
+      <div id='author'><p>- {quote.author}</p></div>
+      <div className='box'>
+      <div ><a id='tweet-quote' href='twitter.com/intent/tweet'><i class="fa-brands fa-twitter-square"></i></a></div>
+      <div id='new-quote'><button onClick={generateQuote}>New Quote</button></div>
+      
+      </div>
+     </div>
     </div>
   );
 }
---
+++ src/quoteGenerator.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export class Quote extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            quote: '',
+            author: ''
+        }
+    }
+
+    getQuote() {
+        
+    }
+}
